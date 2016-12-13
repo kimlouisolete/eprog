11dd987d99c9e5305e9d697d692b1a665dee3cbf
kimlouisolete <kimlouisolete@gmail.com>
2016-12-13 08:39:13 +0800

    Create pythagorean_theorem.c

---
+++ pythagorean_theorem.c
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <math.h>
+
+
+main ()
+{
+	int a;
+	int b;
+	int c;
+	
+	printf("Enter number for a:  ");
+	scanf("%d", &a);
+	
+	printf("Enter number for b:  ");
+	scanf("%d", &b);
+	
+	printf("%d", c = sqrt((a*a)+(b*b)));
+}
+	
+	
