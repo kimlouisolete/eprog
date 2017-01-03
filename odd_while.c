54c864a64974799c7898bcbd0beaba7e07db6688
kimlouisolete <kimlouisolete@gmail.com>
2017-01-03 09:09:57 +0800

    Create odd_while.c

---
+++ odd_while.c
@@ -0,0 +1,12 @@
+#include <stdio.h>  
+   
+int main() {  
+    int odd; 
+    odd = 1; 
+    
+    while (odd <= 100) {   
+		printf("%d ", odd);     
+        odd = odd + 2;  
+    }
+    return 0; 
+} 
